c556b71dc5c5edc87a3500af2004748bd29395d8
vtarnopolskyi <vtarnopolskyi@deloitte.com>
2025-03-08 18:20:33 +0200

    replaced requests to mock

--- src/app/pages/requests/requests.service.ts
+++ src/app/pages/requests/requests.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { IRequest, IRequests } from '../../common/models/base.model';
-import { REQUEST_DATA } from './requests.mocks';
+import { REQUEST_DATA, REQUESTS_DATA } from './requests.mocks';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,8 @@ export class RequestsService {
   http = inject(HttpClient);
 
   getRequests(): Observable<IRequests[]> {
-    return this.http.get<IRequests[]>('http://127.0.0.1:8000/compensation_requests')
+    // return this.http.get<IRequests[]>('http://127.0.0.1:8000/compensation_requests');
+    return of(REQUESTS_DATA);
   }
 
   getRequest(id: string): Observable<IRequest[]> {
